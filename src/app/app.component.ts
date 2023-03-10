3e9c5c31e29dc391aaa9f65bfb8c4c2be4e8932d
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-03-10 20:16:28 -0300

    definindo perfis de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buddyCarer';
+  inLogin = false;
+  ClientLogged = true;
+  AdminLogged = false;
 }
