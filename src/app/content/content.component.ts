7ad6b64ae08f605d56f6b0fdf06edbfe594a5fba
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-03-27 21:05:57 -0300

    myPets

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ɵɵNgOnChangesFeature } from "@angular/core";
 
 @Component({
   selector: "app-content",
@@ -7,4 +7,56 @@ import { Component, Input } from "@angular/core";
 })
 export class ContentComponent {
   @Input() page = "initial";
+
+  myPets: any;
+
+  ngOnChanges(){
+    //Carregando os elementos das páginas de forma dinamica para melhorar a performance
+
+    switch(this.page){
+      case 'myPets':
+        this.myPets = [
+          {
+            nome: 'Barnei',
+            imagem: 'https://images.vexels.com/media/users/3/210964/isolated/preview/9b8f69a5180ae3b60f65d65612db7bda-cachorro-sorrindo-feliz-ilustracao.png'
+          },
+          {
+            nome: 'Ted',
+            imagem: 'https://images.vexels.com/media/users/3/210964/isolated/preview/9b8f69a5180ae3b60f65d65612db7bda-cachorro-sorrindo-feliz-ilustracao.png'
+          },
+          {
+            nome: 'Nero',
+            imagem: 'https://images.vexels.com/media/users/3/210964/isolated/preview/9b8f69a5180ae3b60f65d65612db7bda-cachorro-sorrindo-feliz-ilustracao.png'
+          }
+        ]
+        break;
+
+      case 'Calendar':
+        break;
+
+      case 'Care':
+        break;
+
+      case 'Money':
+        break;
+
+      case 'Shop':
+        break;
+  
+      case 'Dashboard':
+        break;
+  
+      case 'Clients':
+        break;
+
+      case 'Package':
+        break;
+    
+      case 'CalendarAdmin':
+        break;
+    
+      case 'mockery':
+        break;
+    }
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from "@angular/core";
-import { LoginService } from "../services/login.service";
+import { LoginService } from "../services/login/login.service";
 
 @Component({
   selector: "app-login",
