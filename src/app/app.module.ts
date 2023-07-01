d3e9e820230161e65d258e9b42744516f7823c1e
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-07-01 18:56:47 -0300

    updateImports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SideBarComponent } from "./side-bar/side-bar.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AngularMaterialModule } from "./angular-material/angular-material.module";
+import { AngularMaterialModule } from "./externalComponents/angular-material/angular-material.module";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { ContentComponent } from "./content/content.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
